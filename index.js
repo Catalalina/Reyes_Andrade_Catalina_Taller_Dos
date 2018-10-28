77cef7a8deeea87a3209cd7c9b448701a43d190b
CatalinaReyes <catalina19@gmail.com>
2018-10-28 14:23:52 -0500

    Base de datos

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("express-handlebars");
-const app =express();
+const app = express();
 
 app.use(express.static("public"));
 
 //decirle al server com construir templates
-app.engine("handlebars",hbs());
+app.engine("handlebars", hbs());
 
 app.set("view engine", "handlebars");
 //____________________________________________________________________________________________________________________________________________________________
@@ -14,25 +14,19 @@ app.set("view engine", "handlebars");
 
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
-const url =  "mongodb://localhost:27017";
+const url = "mongodb://localhost:27017";
 const mongoName = "Taller2";
 const mongoClient = new MongoClient(url);
-mongoClient.connect(function(err){
 
-assert.equal(null,err);
-console.log("equis de");
+var products = null;
 
-const db = mongoClient.db(mongoName);
-const product =db.collection("cargo");
-product.insertMany([
-    {a:1}, {a:2},{a:3}
+mongoClient.connect(function (err) {
 
-],function(err, result){
+    assert.equal(null, err);
+    console.log("equis de");
 
-    assert.equal(err,null);
-    console.log("se enviooo");
-});
-    mongoClient.close();
+    const db = mongoClient.db(mongoName);
+    products = db.collection("products");
 });
 
 
@@ -42,34 +36,33 @@ product.insertMany([
 //____________________________________________________________________________________________________________________________________________________________
 
 //llamar la dir
-app.get("/",function(request, response){
-    var title= {
+app.get("/", function (request, response) {
+    var title = {
         title: 'A VIBRANT & EXOTIC TEA',
-      
+
     }
-response.render("index", title);
+    response.render("index", title);
 });
 
 //otras cosas
 //app.get("/",function())
 
+app.get("/store", function (request, response) {
+
+    products.find({}).toArray(function(err, docs){
+        if(err){
+            console.log(err);
+            return;
+        }
+
+        var context = {products: docs};
+        response.render("store", context);
+    });
+
+   
+});
 
 
 
 
 app.listen(5500);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ xd.js
@@ -0,0 +1,180 @@
+.insertMany([
+    {
+        nombre: "COCONUT DRINK",
+        imagen: "/Imagenes/products/milkBlueFront.jpg",
+        imagenDos: "/Imagenes/products/milkBlueBack.jpg",
+        imagenTres: "/Imagenes/products/milkBlueLeft.jpg",
+        imagenCuatro: "/Imagenes/products/milkBlueRight.jpg",
+        precio: 9400,
+        favorito: 80,
+        color: "blue",
+        tipo: "milk"
+    },
+
+    {
+        nombre: "GINGER & LEMON TEA",
+        imagen: "/Imagenes/products/teaOrange.jpg",     
+        precio: 3600,
+        favorito: 30,
+        color: "orange",
+        tipo: "tea"
+    },
+
+    {
+        nombre: "PEANUTS DOYPACK WILH HIMALAYAN SALT",
+        imagen: "/Imagenes/products/bagPink.jpg",     
+        precio: 4200,
+        favorito: 20,
+        color: "pink",
+        tipo: "snack"
+    },
+
+    {
+        nombre: "MIXED BARS PACK",
+        imagen: "/Imagenes/products/boxColorfull.jpg",     
+        imagenDos: "/Imagenes/products/boxColorfullTwo.jpg",    
+        imagenTres: "/Imagenes/products/snackColorfull.jpg",    
+        precio: 5800,
+        favorito: 90,
+        color: "red",
+        tipo: "snack"
+    },
+
+    {
+        nombre: "YUZU & CAMOMILE FLAVORED GREEN TEA",
+        imagen: "/Imagenes/products/teaGreen.jpg",     
+        precio: 3600,
+        favorito: 40,
+        color: "green",
+        tipo: "tea"
+    },
+
+    {
+        nombre: "LILAC CHERRY BLOSSOM TEA",
+        imagen: "/Imagenes/products/teaPurple.jpg",     
+        precio: 3600,
+        favorito: 40,
+        color: "purple",
+        tipo: "tea"
+    },
+
+    {
+        nombre: "VANILLA ALMOND DRINK",
+        imagen: "/Imagenes/products/milkPupleFront.jpg",
+        imagenDos: "/Imagenes/products/milkPupleBack.jpg",
+        imagenTres: "/Imagenes/products/milkPupleLeft.jpg",
+        imagenCuatro: "/Imagenes/products/milkPupleRight.jpg",
+        precio: 9400,
+        favorito: 10,
+        color: "Puple",
+        tipo: "milk"
+    },
+
+    {
+        nombre: "QUINOA POPS DOYPACK",
+        imagen: "/Imagenes/products/bagBlue.jpg",     
+        precio: 4200,
+        favorito: 90,
+        color: "blue",
+        tipo: "snack"
+    },
+
+    {
+        nombre: "BLACK TEA & LIME",
+        imagen: "/Imagenes/products/teaBlack.jpg",     
+        precio: 3600,
+        favorito: 40,
+        color: "black",
+        tipo: "tea"
+    },
+
+    {
+        nombre: "CHIA SEEDS & MANGO BAR",
+        imagen: "/Imagenes/products/boxYellow.jpg",    
+        imagenDos: "/Imagenes/products/snackYellow.jpg",    
+        precio: 4200,
+        favorito: 60,
+        color: "yellow",
+        tipo: "snack"
+    },
+
+    {
+        nombre: "ROSES & LYCHEE TEA",
+        imagen: "/Imagenes/products/teaPink.jpg",      
+        precio: 3600,
+        favorito: 10,
+        color: "pink",
+        tipo: "tea"
+    },
+
+    {
+        nombre: "FRUITS & VEGETABLE BAR",
+        imagen: "/Imagenes/products/snackRed.jpg",     
+        imagenDos: "/Imagenes/products/boxRed.jpg",       
+        precio: 4200,
+        favorito: 10,
+        color: "red",
+        tipo: "snack"
+    },
+
+    {
+        nombre: "BLUE POMEGRANATE TEA",
+        imagen: "/Imagenes/products/teasBlue.jpg",     
+        imagenDos: "/Imagenes/products/teaBlue.jpg",       
+        imagenTres: "/Imagenes/products/teaBlueF.jpg",   
+        precio: 3600,
+        favorito: 70,
+        color: "blue",
+        tipo: "tea"
+    },
+
+    {
+        nombre: "ACAI & PROTEN BAR",
+        imagen: "/Imagenes/products/boxPurple.jpg",     
+        imagenDos: "/Imagenes/products/snackPurple.jpg",       
+        precio: 4200,
+        favorito: 95,
+        color: "purple",
+        tipo: "snack"
+    },
+
+    {
+        nombre: "YELLOW STARFRUIT & LOTUS BLOSSOM TEA",
+        imagen: "/Imagenes/products/teaYellow.jpg",         
+        precio: 4200,
+        favorito: 45,
+        color: "yellow",
+        tipo: "tea"
+    },
+
+    {
+        nombre: "WHITE TEA & MANGOSTEEN ",
+        imagen: "/Imagenes/products/teaWhite.jpg",     
+        imagenDos: "/Imagenes/products/teasWhite.jpg",       
+        imagenTres: "/Imagenes/products/teaWhiteF.jpg",   
+        precio: 3600,
+        favorito: 70,
+        color: "white",
+        tipo: "tea"
+    },
+
+    {
+        nombre: "CLUSTER DOYPACK",
+        imagen: "/Imagenes/products/bagYellow.jpg",           
+        precio: 4200,
+        favorito: 60,
+        color: "green",
+        tipo: "snack"
+    },
+
+    {
+        nombre: "QUINOA POPS SMALLPACK",
+        imagen: "/Imagenes/products/sanckBlue.jpg",           
+        precio: 4200,
+        favorito: 50,
+        color: "blue",
+        tipo: "snack"
+    },
+
+
+]);
